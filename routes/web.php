2956b9a487a619056eccc7cbbf237706672d2f15
voltkigisa <raden.233040043@mail.unpas.ac.id>
2024-03-21 18:06:20 +0700

    routing, add file di view, menambahkan css,js,dan img

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "nama" => "Raden Indra Prawirajaya",
+        "email"=> "radenprawirajaya@gmail.com",
+        "image"=> "img/pasfoto.jpg",
+        ]);
+});
+
+Route::get('/posts', function () {
+    return view('posts');
 });
