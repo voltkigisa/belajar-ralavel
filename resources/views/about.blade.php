7593abd61f5a2f74142f38601aff8fe0ee9f8979
voltkigisa <raden.233040043@mail.unpas.ac.id>
2024-03-23 00:06:25 +0700

    belajar models, controller, dan collection

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,10 +2,10 @@
 
 @section('container')
     <h1>Halaman About</h1>
-    <img src="{{ $image}}" alt="{{ $nama}}">
+    <img src="{{ $image}}" alt="{{ $name}}" class="img-tumbnail rounded-circle">
 
     <ul>
-        <li><h3>{{ $nama }}</h3></li>
+        <li><h3>{{ $name }}</h3></li>
         <li><h3>{{ $email }}</h3></li>
     </ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\postController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,75 +25,13 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
-        "nama" => "Raden Indra Prawirajaya",
+        "name" => "Raden Indra Prawirajaya",
         "email"=> "radenprawirajaya@gmail.com",
         "image"=> "img/pasfoto.jpg",
         ]);
 });
 
-$blog_posts = [
-    "title" => "Judul Post Pertama",
-    "author" => "Raden Indra Prawirajaya",
-    "Body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque earum doloremque commodi vel cupiditate, veniam blanditiis harum ea? Sunt adipisci ipsum aperiam quae debitis eveniet, ut quam eius eos amet, nulla delectus. Iste similique, qui vel facilis eum incidunt. Nesciunt, inventore molestiae dolor beatae aliquid quo blanditiis quidem iusto fuga!",
-];
-$blog_posts = [
-    "title" => "Judul Post Kedua",
-    "author" => "Ananda Rizki Maulana",
-    "Body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque earum doloremque commodi vel cupiditate, veniam blanditiis harum ea? Sunt adipisci ipsum aperiam quae debitis eveniet, ut quam eius eos amet, nulla delectus. Iste similique, qui vel facilis eum incidunt. Nesciunt, inventore molestiae dolor beatae aliquid quo blanditiis quidem iusto fuga!",
-];
 
-Route::get('/posts', function () {
-    $blog_posts = [[
-        "title" => "Judul Post Pertama",
-        "slug" => "Judul-Post-Pertama",
-        "author" => "Raden Indra Prawirajaya",
-        "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque earum doloremque commodi vel cupiditate, veniam blanditiis harum ea? Sunt adipisci ipsum aperiam quae debitis eveniet, ut quam eius eos amet, nulla delectus. Iste similique, qui vel facilis eum incidunt. Nesciunt, inventore molestiae dolor beatae aliquid quo blanditiis quidem iusto fuga!",
-    ],
-    [
-        "title" => "Judul Post Kedua",
-        "slug" => "Judul-Post-Kedua",
-        "author" => "Ananda Rizki Maulana",
-        "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque earum doloremque commodi vel cupiditate, veniam blanditiis harum ea? Sunt adipisci ipsum aperiam quae debitis eveniet, ut quam eius eos amet, nulla delectus. Iste similique, qui vel facilis eum incidunt. Nesciunt, inventore molestiae dolor beatae aliquid quo blanditiis quidem iusto fuga!",
-    ]
-    ];
-    
-    return view('posts', [
-        "title"=> "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
+Route::get('/posts',[PostController::class, 'index']);
 //halaman single post
-Route::get('posts/{slug}', function($slug){
-
-    $blog_posts = [[
-        "title" => "Judul Post Pertama",
-        "slug" => "Judul-Post-Pertama",
-        "author" => "Raden Indra Prawirajaya",
-        "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque earum doloremque commodi vel cupiditate, veniam blanditiis harum ea? Sunt adipisci ipsum aperiam quae debitis eveniet, ut quam eius eos amet, nulla delectus. Iste similique, qui vel facilis eum incidunt. Nesciunt, inventore molestiae dolor beatae aliquid quo blanditiis quidem iusto fuga!",
-    ],
-    [
-        "title" => "Judul Post Kedua",
-        "slug" => "Judul-Post-Kedua",
-        "author" => "Ananda Rizki Maulana",
-        "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque earum doloremque commodi vel cupiditate, veniam blanditiis harum ea? Sunt adipisci ipsum aperiam quae debitis eveniet, ut quam eius eos amet, nulla delectus. Iste similique, qui vel facilis eum incidunt. Nesciunt, inventore molestiae dolor beatae aliquid quo blanditiis quidem iusto fuga!",
-    ]
-    ];
-
-    $new_post= [];
-    foreach($blog_posts as $post){
-        if($post["slug"] === $slug){
-            $new_post =$post;
-        }
-    }
-
-    if($new_post === []){
-        abort(404);
-    }
-
-
-    return view('post', [
-        'title'=> 'single post',
-        'post'=> $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
